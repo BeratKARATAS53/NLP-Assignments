a9c27be5cbc3f3f81ee823482fe6168bfefaea57
BeratKARATAS53 <b21527142@cs.hacettepe.edu.tr>
2020-05-31 23:38:59 +0300

    Poems Generated

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -10,13 +10,14 @@ import dynet_config
 dynet_config.set_gpu()
 
 import nltk
-from nltk import FreqDist
-from nltk.util import ngrams
-from nltk.tokenize import RegexpTokenizer, word_tokenize
-from collections import Counter
 # nltk.download('punkt')
 
+from collections import Counter
+from nltk.tokenize import RegexpTokenizer, word_tokenize
+from nltk.util import ngrams
+from nltk import FreqDist
 
+file = open('poems.txt', 'w')
 class FNN:
     def __init__(self):
         print("Init")
@@ -24,12 +25,13 @@ class FNN:
         self.word_to_index = {}
         self.index_to_vocab = {}
         self.vocab_to_index = {}
+        self.unique_words = []
         self.word_vectors = {}
-        
+
         self.unim_poem = []
         self.poems = []
         self.mini_poems = []
-        
+
         self.word_list = []
         self.words_count = []
         self.bigram_poem = []
@@ -43,7 +45,6 @@ class FNN:
                 line = line.split()
                 self.word_to_index[line[0]] = len(self.embeddings)
                 self.embeddings.append(np.array(line[1:], dtype=np.float32))
-
 
     def poem_file(self, folder_path):
         print("Read Poem")
@@ -60,7 +61,6 @@ class FNN:
                     each["poem"] = each["poem"].replace("\n", " eol ")
                     each["poem"] = "bos " + each["poem"] + " eos"
                     self.poems.append(each["poem"])
-        
 
     def vocabulary(self):
         print("Vocabulary")
@@ -74,84 +74,130 @@ class FNN:
                         self.word_vectors[token] = np.random.rand(50)
                     self.vocab_to_index[token] = len(self.word_vectors)
                     self.index_to_vocab[len(self.word_vectors)] = token
-            
+                    self.unique_words.append(token)
 
     def bigram(self):
         print("Bigram")
         for each_poem in self.poems:
             nltk_tokens = nltk.word_tokenize(each_poem)
             self.bigram_poem += list(nltk.bigrams(nltk_tokens))
-        print(self.bigram_poem)
-    # def bigram_counts(self):
-    #     print("Bigram Count")
-    #     for poem in self.bigram_poem:
-    #         fdist = nltk.FreqDist(poem)
-    #         for k, v in fdist.items():
-    #             self.bigram_count[k] = v
 
-    # def calculate_bigramProbs(self):
-    #     print("Bigram Prob")
-    #     for k, v in self.bigram_count.items():
-    #         first_word = k[0]
-    #         bigram_prob = (
-    #             self.bigram_count[k] + 1) / (self.words_count[first_word] + len(self.words_count))
-    #         self.bigram_prob[k] = bigram_prob
+    def model(self):
+        hidden_size = 64
+        vocabulary_size = len(self.vocab_to_index)
+        input_size = output_size = vocabulary_size
 
-    # def model(self):
-    #     hidden_size = 64
-    #     vocabulary_size = len(self.words_count)
-    #     input_size = output_size = vocabulary_size
+        m = dy.Model()
 
-    #     m = dy.Model()
+        W = m.add_parameters((hidden_size, input_size))
+        b = m.add_parameters(hidden_size)
+        V = m.add_parameters((output_size, hidden_size))
+        a = m.add_parameters(output_size)
+
+        x = dy.vecInput(input_size)
+        y = dy.vecInput(output_size)
+        h = dy.tanh((W * x) + b)
+        output = dy.softmax(V*h)
+
+        y_pred = h
+
+        loss = dy.squared_distance(output, y)
+        trainer = dy.SimpleSGDTrainer(m)
+
+        for iter in range(1):
+            mloss = 0.0
+            seen_instances = 0
+            for word_pair in self.bigram_poem:
+                x.set(self.word_vectors[word_pair[0]])
+                y.set(self.word_vectors[word_pair[1]])
+                seen_instances += 1
+
+                mloss += loss.scalar_value()
+                loss.forward()
+                loss.backward()
+                trainer.update()
+
+                if (seen_instances > 1 and seen_instances % 100 == 0):
+                    print(seen_instances, "/", len(self.bigram_poem),
+                          "***average loss is:", mloss / seen_instances)
+
+            print(mloss / seen_instances)
+
+    def predict(self, word):
+        hidden_size = 64
+        vocabulary_size = len(self.vocab_to_index)
+        input_size = output_size = vocabulary_size
+
+        m = dy.Model()
+
+        W = m.add_parameters((hidden_size, input_size))
+        b = m.add_parameters(hidden_size)
+        V = m.add_parameters((output_size, hidden_size))
+        a = m.add_parameters(output_size)
+
+        x = dy.vecInput(input_size)
+        y = dy.vecInput(output_size)
+        h = dy.tanh((W * x) + b)
+        output = dy.softmax(V*h)
+
+        x.set(self.word_vectors[word])
+        probs = output.npvalue()
+
+        predicted_idx = np.random.choice(
+            self.unique_words, p=(probs+0.001)/sum(probs+0.001))
+
+        return predicted_idx
+
+    
+    def generation(self, number_of_line):
+        start_token = "bos"
+        while number_of_line > 0:
+            each_line = []
+            word = 20
+            while word > 0:
+                if(word == 20):
+                    next_word = self.predict(start_token)
+                    each_line.append(next_word)
+                    if(next_word == "eol"):
+                        break
+                    if(next_word == "eos"):
+                        return
+                else:
+                    next_word = self.predict(each_line[-1])
+                    each_line.append(next_word)
+                    if(next_word == "eol"):
+                        break
+                    if(next_word == "eos"):
+                        return
+                word -= 1
+            number_of_line -= 1
+
+            each_line = ' '.join([str(word) for word in each_line])
+            file.write(each_line+"\n")
+        file.write("\n")
+
+    
+    # def perplexity(self):
+    #     sprob_dict = sprob(sentence)
+    #     log_probs_bi = sprob_dict["bigram"]
+    
+    #     len_bi = len(bigramSProbs)
+    #     HW_bi = (-1/len_bi) * log_probs_bi
+    #     perplexity_bi = math.exp(HW_bi)
         
-    #     W = m.add_parameters((hidden_size, input_size))
-    #     b = m.add_parameters(hidden_size)
-    #     V = m.add_parameters((output_size, hidden_size))
-    #     a = m.add_parameters(output_size)
-        
-    #     x = dy.vecInput(input_size)
-    #     y = dy.vecInput(output_size)
-    #     h = dy.tanh((W * x) + b)
-    #     output = dy.logistic(V*h)
-        
-    #     y_pred = h
-
-    #     loss = dy.squared_distance(output, y)
-    #     trainer = dy.SimpleSGDTrainer(m)
-        
-    #     for iter in range(1):
-    #         mloss = 0.0
-    #         seen_instances = 0
-    #         for poem in self.bigram_poem:
-    #             for word_pair in poem:
-    #                 print(self.embeddings_dict.get(word_pair[0]))
-    #                 x.set(self.embeddings_dict.get(word_pair[0]))
-    #                 y.set(self.embeddings_dict.get(word_pair[1]))
-    #                 seen_instances += 1
-
-    #                 mloss += loss.scalar_value()
-    #                 loss.forward()
-    #                 loss.backward()
-    #                 trainer.update()
-
-    #                 if (seen_instances > 1 and seen_instances % 100 == 0):
-    #                     print(seen_instances, "/", len(self.bigram_poem), "***average loss is:", mloss / seen_instances)
-
-    #         print( mloss / seen_instances)
-
+    #     perplexity_dict["bigram"] = perplexity_bi
 
 neuralNetwork = FNN()
 neuralNetwork.glove_file("./Assignment4/glove.6B/glove.6B.50d.txt")
 neuralNetwork.poem_file("./Assignment4/unim_poem.json")
 
-# neuralNetwork.preprocessing()
 neuralNetwork.bigram()
-# neuralNetwork.vocabulary()
-# neuralNetwork.bigram_counts()
-# neuralNetwork.calculate_bigramProbs()
+neuralNetwork.vocabulary()
 
-# rand = random.choice(neuralNetwork.word_list)
-# print(rand)
-# print(neuralNetwork.embeddings_dict[rand])
+neuralNetwork.model()
 
-# print(neuralNetwork.model())
+number_of_poems = 5
+number_of_lines = 4
+while number_of_poems > 0:
+    neuralNetwork.generation(number_of_lines)
+    number_of_poems -= 1
